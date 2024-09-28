42ea7be1cbb0362c814c0f938a61f1115b004cae
karolsw3 <karol.sw3@gmail.com>
2024-09-29 00:05:49 +0200

    feat: make sidebar collapsible

--- web/src/components/HistorySidebar.tsx
+++ web/src/components/HistorySidebar.tsx
@@ -1,8 +1,10 @@
 import { useApiCommunicatorStore } from '../modules/ApiCommunicator/ApiCommunicatorStore.ts'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import classNames from 'classnames'
 
 const HistorySidebar = () => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
 	const fetchConversations = useApiCommunicatorStore((state) => state.fetchConversations);
 	const currentlyOpenConversationId = useApiCommunicatorStore((state) => state.currentlyOpenConversationId);
 	const setCurrentlyOpenConversationId = useApiCommunicatorStore((state) => state.setCurrentlyOpenConversationId);
@@ -18,20 +20,53 @@ const HistorySidebar = () => {
 	
 	return (
 		<div
-			className={'h-screen w-64 border-r border-neutral-300 py-2 bg-gov-light-gray shadow-md'}
+			className={'relative'}
 		>
-			{conversations.map(conversation => (
-				<button
+			<button
+				aria-checked={isSidebarOpen}
+				aria-label={'Open/Close Sidebar'}
+				onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+				className={classNames(
+					'absolute top-0 right-0 -mr-4 mt-3',
+					'w-8 h-8 bg-white z-20 shadow-xs',
+					'border border-neutral-300 rounded-full',
+					'flex items-center justify-center',
+					'hover:bg-gov-light-gray duration-75 active:bg-neutral-300'
+				)}
+			>
+				{ isSidebarOpen ? '>':'<' }
+			</button>
+			<div
+				className={classNames(
+					'relative overflow-hidden transition-all duration-100',
+					isSidebarOpen ? 'w-10' : 'w-64'
+				)}
+			>
+				<div
 					className={classNames(
-						'w-full px-4 py-2',
-						'mb-2',
-						currentlyOpenConversationId === conversation._id ? 'bg-gov-blue text-white':'hover:text-gov-blue'
+						'h-screen w-64 border-r border-neutral-300',
+						'py-2 bg-gov-light-gray shadow-md overflow-hidden',
+						'relative',
 					)}
-					onClick={() => handleClickHistoryEntry(conversation._id)}
 				>
-					{ conversation._id }
-				</button>
-			))}
+					<div
+						className={'mt-6'}
+					>
+					</div>
+					{conversations.map(conversation => (
+						<button
+							className={classNames(
+								'w-full px-4 py-2',
+								'mb-2',
+								currentlyOpenConversationId === conversation._id ? 'bg-gov-blue text-white' : 'hover:text-gov-blue'
+							)}
+							onClick={() => handleClickHistoryEntry(conversation._id)}
+						>
+							{ conversation._id }
+						</button>
+					))}
+				</div>
+			</div>
 		</div>
 	)
 }
