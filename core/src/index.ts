7f204a7a63e5358906548acf5060dda5e80adf3a
Kacper Barcik <45320966+kilofeq@users.noreply.github.com>
2024-09-28 12:56:38 +0200

    Initial commit for web

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,8 +1,19 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { swagger } from '@elysiajs/swagger'
 
 const app = new Elysia()
-  .get("/", () => "Hello Elysia")
+  .use(swagger)
+  .get("/", () => "Hello Elysia", {
+    body: t.Object({
+      message: t.String({
+        maxLength: 100,
+        minLength: 0
+      })
+    })
+  })
   .listen(3000);
+
+export type CoreApp = typeof app
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
